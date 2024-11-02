19a182c1ea001640882fadbee4b84a4e143193b4
Muhammad Junaid <junaidchohanedu@gmail.com>
2024-11-02 20:12:52 +0500

    Setting color using colorScheme cardTheme, elevatedButtonTheme, textTheme

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:expenses__app/widgets/expense.dart';
 import 'package:flutter/material.dart';
 
-
-var kColorScheme = ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 96, 61, 181),);
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 96, 61, 181),
+);
 
 void main() {
   runApp(const MyApp());
@@ -14,16 +15,35 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData().copyWith(colorScheme: kColorScheme,
-      appBarTheme: const AppBarTheme().copyWith(
-        backgroundColor: kColorScheme.onPrimaryContainer,
-        foregroundColor: kColorScheme.primaryContainer,
-      )
+      theme: ThemeData().copyWith(
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+        cardTheme: const CardTheme().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 8,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer,
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+          titleLarge: TextStyle(
+            fontWeight: FontWeight.normal,
+            color: kColorScheme.onSecondaryContainer,
+            fontSize: 14,
+          )
+        ),
       ),
       home: const Expenses(),
     );
   }
 }
-
